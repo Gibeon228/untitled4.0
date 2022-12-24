72cb4c40b968b0e27ad320a6eee5f85825a0d4e3
Vladimir <VL97@mail.ru>
2022-12-24 17:54:42 +0300

    ООП 6 дз семинар

--- src/main/java/chars/Healer.java
+++ src/main/java/chars/Healer.java
@@ -16,9 +16,47 @@ public abstract class Healer extends NPC{
     public void step(List<NPC> team) {
         if (getState() == States.DEAD) return;
         setState(States.ATTACK);
-        int index = findMinHealth();
+        int index = findMinHealthEnemy(team);
+        if (index < getDamage()[0]) {
+            team.get(index).setState(States.DEAD);
+            return;
+        }
+        index = findMinHealth();
         NPC teamMateNeedHealth = getMyTeam().get(index);
-        teamMateNeedHealth.setHealth(teamMateNeedHealth.getHealth() - getDamage()[0]);
+        if (teamMateNeedHealth.getHealth() / teamMateNeedHealth.getMaxHealth() < 0.75) {
+            teamMateNeedHealth.setHealth(teamMateNeedHealth.getHealth() - getDamage()[0]);
+        } else if (revive() > -1) {
+            revive();
+        } else  if (teamMateNeedHealth.getHealth() / teamMateNeedHealth.getMaxHealth() < 1) {
+            teamMateNeedHealth.setHealth(teamMateNeedHealth.getHealth() - getDamage()[0]);
+        } else  {
+            team.get(findMinHealthEnemy(team)).setHealth(teamMateNeedHealth.getHealth() + getDamage()[0]);
+        }
+
+    }
+
+    private int revive() {
+        for (NPC teamMate : getMyTeam()) {
+            if (teamMate.getState() == States.DEAD && !teamMate.getClass().toString().contains("Peasant"))
+            {
+                teamMate.setHealth(1);
+                teamMate.setState(States.ALIVE);
+                return 0;
+            }
+        }
+        return -1;
+    }
+
+    private int findMinHealthEnemy(List<NPC> team) {
+        double minHealth = Double.MAX_VALUE;
+        int index = -1;
+        for (int i = 0; i < getMyTeam().size(); i++) {
+            if ((team.get(i).getHealth() < minHealth) && team.get(i).getState() != States.DEAD) {
+                minHealth = team.get(i).getHealth();
+                index = i;
+            }
+        }
+        return index;
     }
 
     private int findMinHealth() {
